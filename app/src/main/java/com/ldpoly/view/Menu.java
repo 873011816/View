f78704f489694a0ff0cbfb89ae95721d9e867d06
aise <873011816@qq.com>
2018-05-30 16:35:42 +0800

    私人菜单

---
+++ app/src/main/java/com/ldpoly/view/Menu.java
@@ -0,0 +1,24 @@
+package com.ldpoly.view;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.ViewGroup;
+
+/**
+ * Created by Administrator on 2018/5/30.
+ */
+
+public class Menu extends ViewGroup {
+    public Menu(Context context) {
+        super(context);
+    }
+
+    public Menu(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    @Override
+    protected void onLayout(boolean b, int i, int i1, int i2, int i3) {
+
+    }
+}
